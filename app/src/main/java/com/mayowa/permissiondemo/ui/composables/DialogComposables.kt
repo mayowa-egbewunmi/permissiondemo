f0b3f2751bad4a56fbab5bdde77fd9887d8a764f
Mayowa Egbewunmi <mayowa@premise.com>
2024-09-20 19:53:17 +0100

    fix: launch settings screen

--- app/src/main/java/com/mayowa/permissiondemo/ui/composables/DialogComposables.kt
+++ app/src/main/java/com/mayowa/permissiondemo/ui/composables/DialogComposables.kt
@@ -6,7 +6,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 
 @Composable
-fun CameraPermissionRationaleDialog(onRequestPermission: () -> Unit) {
+fun CameraPermissionRationaleDialog(onRequestPermission: () -> Unit, onClose: () -> Unit) {
     AlertDialog(
         onDismissRequest = { /* Do nothing */ },
         title = { Text(text = "Camera Permission Required") },
@@ -17,7 +17,26 @@ fun CameraPermissionRationaleDialog(onRequestPermission: () -> Unit) {
             }
         },
         dismissButton = {
-            Button(onClick = { /* Handle cancel action */ }) {
+            Button(onClick = onClose) {
+                Text("Cancel")
+            }
+        }
+    )
+}
+
+@Composable
+fun CameraPermissionSettingsDialog(onSettingsTapped: () -> Unit, onClose: () -> Unit) {
+    AlertDialog(
+        onDismissRequest = { /* Do nothing */ },
+        title = { Text(text = "Camera Permission Required") },
+        text = { Text(text = "We need access to your camera to capture photos. Please enable camera access in your device settings.") },
+        confirmButton = {
+            Button(onClick = onSettingsTapped) {
+                Text("Launch Settings")
+            }
+        },
+        dismissButton = {
+            Button(onClick = onClose) {
                 Text("Cancel")
             }
         }
