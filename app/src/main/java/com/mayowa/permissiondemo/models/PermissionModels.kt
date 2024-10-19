c0a4396e742d272323872ee56e17ff6184e20967
Mayowa Egbewunmi <mayowa@premise.com>
2024-10-19 18:22:22 +0100

    feat: non reusable permission flow

--- app/src/main/java/com/mayowa/permissiondemo/models/PermissionModels.kt
+++ app/src/main/java/com/mayowa/permissiondemo/models/PermissionModels.kt
@@ -9,16 +9,22 @@ import com.mayowa.permissiondemo.ui.screens.EntryScreenViewModel
 val PERMISSION_RATIONALE = mapOf(
     Manifest.permission.CAMERA to "We need access to your camera to capture photos.",
     Manifest.permission.ACCESS_MEDIA_LOCATION to "We need access to your media location to organize your photos by location.",
+    Manifest.permission.READ_MEDIA_IMAGES to "We need access to your media images to display them in the app.",
+    Manifest.permission.READ_EXTERNAL_STORAGE to "We need access to your media images to display them in the app.",
 )
 
 val PERMISSION_TITLE = mapOf(
-    Manifest.permission.CAMERA to "Camera Permission", // TODO: Use resource
-    Manifest.permission.ACCESS_MEDIA_LOCATION to "Media Location Permission", //TODO: Add icon
+    Manifest.permission.CAMERA to "Camera Permission",
+    Manifest.permission.ACCESS_MEDIA_LOCATION to "Media Location Permission",
+    Manifest.permission.READ_MEDIA_IMAGES to "Media Images Permission",
+    Manifest.permission.READ_EXTERNAL_STORAGE to "Media Images Permission",
 )
 
 val PERMISSION_ICONS = mapOf(
     Manifest.permission.CAMERA to R.drawable.ic_art_camera,
-    Manifest.permission.ACCESS_MEDIA_LOCATION to R.drawable.ic_art_camera, //TODO: Add icon
+    Manifest.permission.ACCESS_MEDIA_LOCATION to R.drawable.ic_art_camera,
+    Manifest.permission.READ_MEDIA_IMAGES to R.drawable.ic_art_camera,
+    Manifest.permission.READ_EXTERNAL_STORAGE to R.drawable.ic_art_camera,
 )
 
 sealed class PermissionAction {
